94431bfa738b0c988836926d3b67973c398f15e6
Kihon <kihonliu@gmail.com>
2024-06-21 15:09:19 +0800

    add lottie dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.lottie)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
